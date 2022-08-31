225fb5517b618b40edb15e18b0466e5a790a4df2
sin91 <sin91@DESKTOP-QAFMKM9>
2022-09-01 01:37:37 +0900

    123123123123

--- front/public/개인과제/JS개인과제/JS중간과제_틱택토/틱택토.js
+++ front/public/개인과제/JS개인과제/JS중간과제_틱택토/틱택토.js
@@ -1,74 +1,97 @@
 
-	
-const board = document.querySelector('#board')
 
+const 게임판 = document.querySelector("#gameboard")
 
-let gamebox = []
+let count = 0
+let 게임상황 = []
 
-function make_board(){	
-	gamebox = [null, null, null, null, null, null, null, null, null]
+function 게임시작(){
+	let gameboard = ''	
+	게임상황 = [null, null, null, null, null, null, null, null, null] 
 	for(let i = 0 ; i<9 ; i++){
-
-		gamebox.push(i)
-		board.innerHTML += `<button id='${i}' onclick='버튼입력(${i})'>${i}</button>`
+		gameboard += `<button id=${i} onclick="입력(${i})">${i}</button>`
 		if(i%3==2){
-			board.innerHTML += '<br>'
+			gameboard += '<br>'
 		}
 	}	
+	게임판.innerHTML = gameboard
 }
 
 
-
-function 버튼입력(i){
-	
-	중복검사(i)
-	
-	
-	gamebox[i] = 'O'
+function 입력(i){
+	count++
+	if(!중복검사(i)){
+		alert('이미 둔 자리입니다.')		// 이새기 왜 안돼?	왜안돼긴 병시나...	
+		return
+	}
+	게임상황[i] = 'O'
 	document.getElementById(i).innerHTML = 'O'
-
-	let rand = Math.floor(Math.random())
+	
+	
+	if(승리판단()){
+		return
+	}
+	console.log(count)
+	if(count==5){
+		alert('무승부')
+		return
+	}
 	
 	
 	while(true){
-		중복검사(i)
-		gamebox[rand] = 'X'
+		let rand = Math.floor(Math.random()*9)
+		if(중복검사(rand)==true){
+		게임상황[rand] = 'X'
 		document.getElementById(rand).innerHTML = 'X'
 		break
+		}
 	}
-
-
-	
+	if(승리판단()){
+		return
+	}
 }
 
 
 function 중복검사(i){
-	
-	if(gamebox[i]!=null){
-		alert('이미 둔 자리입니다!')
-		return 
+
+	if(게임상황[i]==null){  //i는 어디서 올까 ?  --> 입력(i) 에서 받아진다
+		return true
+	}else{
+		return false
 	}
-	
 }
 
+//승리하는 경우의 수 or 조건 찾기
 
+//가로세로 길이가 달라도 적용 되도록 만들기
 
+//가로 승리조건 : [012, 345, 678], 세로 승리조건 : [036, 147, 258], 대각선 승리 조건 : [048, 246]
 
+//콜백함수 공부 / 연산자 값 산출 공부!!!!!!!!@!!!!@!@!@!@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function 승리판단(){ // 매개변수 필요 없을거 가튼디..? 아니네 ㅋㅋ 필요없어 븅시나...
+	for(let i = 0 ; i<=6 ; i += 3){
+		if(게임상황[i]!=null && 게임상황[i] == 게임상황[i+1] && 게임상황[i+1] ==게임상황[i+2]){        // 왜 게임상황[i] == 게임상황[i+1] && 게임상황[i+1] == 게임상황[i+2]이어야 하는지 
+			alert(게임상황[i]+'승리')										//이렇게 알림 바로 띄우고 종료하면 안되는지
+			return true													//return해도 승리 판단 의 리턴값 -> 입력(i)의 파라미터로 돌아갈 뿐 입력(i)가 멈추진 않음
+		}
+	}for(let i = 0; i<=2 ; i++){
+		if(게임상황[i]!=null && 게임상황[i]==게임상황[i+3] && 게임상황[i+3]==게임상황[i+6]){  // == 연산자는 3개 값 동시 비교 못하는듯 함. && || 연산시 앞뒤 값 출력되듯 뭐가 있는듯
+			alert(게임상황[i]+'승리')
+			return true
+		}
+	}
+	let i = 0
+	if(게임상황[i]!=null && 게임상황[i]==게임상황[i+4] && 게임상황[i+4]==게임상황[i+8]){	
+		alert(게임상황[i]+'승리')
+		return true
+	}
+	i = 2 
+	if(게임상황[i]!=null && 게임상황[i]==게임상황[i+2] && 게임상황[i+2]==게임상황[i+4]){	
+		alert(게임상황[i]+'승리')
+		return true
+	}
+}
 
 
 
---
+++ front/public/개인과제/JS개인과제/JS중간과제_틱택토/틱텍토2.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+ 
+ 
+ const 게임판 = document.querySelector('#게임판')			// -> 게임판 DIV 불러오기
+ 
+ //비욘세 개쩌러!!! PURE/HONEY 들으면서 하세요~ HATED 도요~~
+ 
+ 
+ 
+ let gameboard = ''				// 게임판을 그릴 변수 선언
+ 
+ function 게임시작(){				// 게임판 그리기 함수
+	for(let i = 0 ; i<9 ; i++){						//반복문을 통해서 
+		gameboard += `<button id=${i} onclick="게임시작(${i})">${i}</button>`		//한줄 입력을 여러번 반복. 게임보드 변수에 더하지 않고 바로 div로 입력 할 수 있으나
+		if(i%3==2){									//버튼을 누를 때마다 게임 내용을 초기화 할 수 있도록 변수에 하여 출력은 누적 더하기가 아닌 대입연산자를 이용하여 할 예정
+			gameboard += '<br>'						//3칸 입력마다 줄바꿈
+		}	
+	}
+	게임판.innerHTML = gameboard						//게임판 HTML에 출력 !!
+}
+ 
+ //주석포기... 완성하고 잘랍니다.
+// 게임판 클릭 시 발생할 이벤트 구현
+
+function 버튼클릭(i){									//1. 함수 선언 // 현재 단계에선 아직 매개변수 필요 없음	
+													
+	document.getElementById(i).innerHTML = 'O'		//2. ID를 식별자로 하여 호출되는 값에 O를 대입. ->게임시작()의 <button>에 id식별자를 추가. 식별해야 값을 바꿔줄 수 있음!!
+	
+													//5. 4에서 생성한 난수를 변수 rand로 선언했으므로 하단 3에서 입력한 getElementById()에 rand 입력 후 1.항목 아랫줄의 6으로 
+	let rand = Math.floor(Math.random()*9)			//4. 0이상 1미만의 난수를 만들어주는 Math.random()에 9를 곱하여 0~8.9999... 의 난수를 만든 후 Math.floor를 이용해 소수점 버림.  
+	document.getElementById(rand).innerHTML = 'X'	//3. 내가 누른 버튼이 O로 바뀐 후 임의의 버튼(상대 플레이어)이 입력되도록 변경. 구분위해 X대입
+}
