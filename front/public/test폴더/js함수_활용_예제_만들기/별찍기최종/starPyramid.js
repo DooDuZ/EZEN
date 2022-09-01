3d89a803b0ec315cea78663cc71df9edf83f896e
sin91 <sin91@DESKTOP-QAFMKM9>
2022-09-02 01:09:45 +0900

    별 절반

--- front/public/test폴더/js함수_활용_예제_만들기/별찍기최종/starPyramid.js
+++ front/public/test폴더/js함수_활용_예제_만들기/별찍기최종/starPyramid.js
@@ -11,28 +11,34 @@
 for(let i = 1 ; i<=5 ; i ++){
 	if(i%2==1){
 		count += 1
-		console.log(count)
 	}for(let j = 1 ; j<=6-count+1 ; j++){
 		output += ' '
 	}for(let s = 1 ; s<=2*count-1 ; s++){
 		output += '*'
-	}output += '\n'
-}
-
-
+	}output += '\n'																//   2 1 0
+}																				// 0 2 3 3
+																				// 1 2 3 4 
+											 									// 0 i i i-1
+											 									
 let count2 = 0
-
-for(let i = 1 ; i<=4 ; i++ ){
-	if(i++){
-		
-	}
-	for(let b = 0 ; b<=2 ; b+=count){
-		output +=' '
-	}
-	for(let s = 1 ; s<= 13-count2 ; s++){
-		output+='*'
+											 									
+for(let i = 0 ; i<4 ; i++){	
+	for(let b = 0 ; b<count2 ; b++){
+		output += ' '		
+	}						
+	for(let s = 1 ; s<=13-2*count2 ; s++){
+	output += '*'
 	}output += '\n'
+	if(i==0){
+		count2 = 2 
+	}else{
+		count2 = 3
+	}
 }
+
+
+console.log(output)
+
 
 /*
 let 수열 = 0
@@ -45,7 +51,7 @@ for(let i = 1 ; i <=4 ; i++){
 
 */
 
-console.log(output)
+
 
 
 /*
