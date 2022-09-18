25b3da92c49cf1d1e47e327d95652d81aca1042c
sin91 <sin91@DESKTOP-QAFMKM9>
2022-09-18 20:37:41 +0900

    com

---
+++ Backend/src/Homework/codingtest/test3.java
@@ -0,0 +1,7 @@
+package Homework.codingtest;
+
+public class test3 {
+	public static void main(String[] args) {
+		
+	}
+}
--- Backend/src/Homework/test2.java
+++ Backend/src/Homework/test2.java
@@ -1,11 +1,30 @@
 package Homework;
 
+import java.util.Scanner;
+
 public class test2 {
-
-	
+	// 한수
 	public static void main(String[] args) {
-		
-
+		Scanner input = new Scanner(System.in);
+		test2 tst = new test2();
+		System.out.println(tst.checker(input.nextInt()));
+	}	
+	int checker(int n) {
+		int count = 0;
+		for(int i = 1 ; i<= n ; i++) {
+			if(i<100) {
+				count++;
+			}else if(i>=100 || i<1000) {
+				int a = i/100;
+				int b = (i-(a*100))/10;
+				int c = i-(a*100)-(b*10);
+				if(a-b == b-c) {
+					count++;
+				}
+			}else if(i==1000) {
+				continue;
+			}
+		}
+		return count;	
 	}
-	
 }
