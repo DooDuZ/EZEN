1c2168b84804091fdae02a31f7297d4ef9a585aa
sin91 <sin91@DESKTOP-QAFMKM9>
2022-09-22 01:29:32 +0900

    7_8진행중

---
+++ Backend/src/Homework/codingtest/test7_8.java
@@ -0,0 +1,16 @@
+package Homework.codingtest;
+
+import java.util.Scanner;
+
+public class test7_8 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("큰수1");
+		String number = input.next();
+		System.out.println("큰수2");
+		String number2 = input.next();
+		
+		
+	}
+}
