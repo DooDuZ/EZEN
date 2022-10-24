2497da03280d998cecb33b26f0c11c8a101e1077
504 <504@DESKTOP-KUT47CM>
2022-10-24 19:12:14 +0900

    test

--- Backend/src/Homework/codingtest/test.java
+++ Backend/src/Homework/codingtest/test.java
@@ -1,13 +1,26 @@
 package Homework.codingtest;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class test {
-	public static void main(String[] args) {
-		try {
-			int s = System.in.read();
-	    	System.out.println(s);
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-    	
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+				
+		String s = input.next();
+		
+		Map<String, String> map = new HashMap<>();
+		map.put("c=", "č");
+		map.put("c-", "ć");
+		map.put("dz=", "dž");
+		map.put("lj", "lj");
+		map.put("nj", "nj");
+		map.put("s=", "š");
+		map.put("z=", "ž");
+		
+		int length = s.length();
+		
+		
 	}
 }
--- Backend/src/Homework/codingtest/test2.java
+++ Backend/src/Homework/codingtest/test2.java
@@ -5,36 +5,10 @@ import java.util.Scanner;
 public class test2 {
 	public static void main(String[] args) {
 		
-		Scanner input = new Scanner(System.in);
-		String s = input.next();
+		char a = 'A';
+		char z = 'Z';
 		
-		int[] arr = new int[26];
-		
-		for(int i = 0 ; i<s.length() ; i++) {
-			if(s.charAt(i)>=65 && s.charAt(i)<=90) {
-				arr[s.charAt(i)-65]++;
-			}else if(s.charAt(i)>=97 && s.charAt(i)<=122){
-				arr[s.charAt(i)-97]++;
-			}
-		}
-		int max = arr[0];
-		int answer = 65;
-		int count = 0;
-		
-		for(int i = 0; i<26 ; i++) {
-			if(arr[i]>max) {
-				max = arr[i];
-				answer = 65+i;
-			}
-		}
-		for(int i = 0 ; i<26 ; i++) {
-			if(arr[i]==max) {
-				count++;
-			}
-		}
-		if(count!=1) {
-			answer = 63;
-		}
-		System.out.println((char)answer);
+		System.out.println((int)a);
+		System.out.println((int)z);
 	}    
 }
